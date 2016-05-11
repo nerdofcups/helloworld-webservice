6849767206738166b18a8e2bd86056872aada6c3
Kelly M <kmayfield@directv.com>
2016-05-10 17:44:51 -0700

    Add Program Files

---
+++ src/org/momcomics/tt/webservice/HelloWorld.java
@@ -0,0 +1,54 @@
+package org.momcomics.tt.webservice;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.att.tutorial.term.assignment1.bytearray.InputSyntaxException;
+import com.att.tutorial.term.assignment1.bytearray.Term;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloWorld() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try (ByteArrayInputStream stream = new ByteArrayInputStream(request.getParameter("term").getBytes())) {
+			Term term = Term.parseTerm(stream);
+			response.getWriter().append(term.toString());
+		} catch (InputSyntaxException e) {
+			throw new ServletException(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
